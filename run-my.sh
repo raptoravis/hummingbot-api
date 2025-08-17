ccbba4b16ee07353a0a984b5abe71a75645fae5c
raptoravis <raptoravis@gmail.com>
2025-08-17 17:28:14 +0800

    add Makefile-my and requirements-my.txt to install and run

--- run-my.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-echo "Running API from source..."
-# Activate conda environment and run with uvicorn
-docker compose up emqx postgres -d
-# source "$(conda info --base)/etc/profile.d/conda.sh"
-# conda activate hummingbot-api
-uvicorn main:app --reload
